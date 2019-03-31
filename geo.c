94167ccf9bff356e7395978b90573722f625a4aa
dimdimich123 <Frostm333@gmail.com>
2019-03-31 13:34:00 +0700

    Added function to calculate the area and perimeter of the triangle

--- geo.c
+++ geo.c
@@ -12,6 +12,7 @@ int Str(int& x1,
         int& xt3,
         int& yt3);
 void circle(int f);
+void triangle(int xt1, int yt1, int xt2, int yt2, int xt3, int yt3);
 const char B[] = "circle", E[] = "exit", T[] = "triangle";
 char A[8], C[8], F[8], D[100], FI1[100], FI2[100], FI3[245], H[8], G[8], J[8];
 int bb;
@@ -23,6 +24,21 @@ void circle(int f)
     printf("plosh=%3.3f\n", ploshs);
     perims = 2 * 3.14 * f;
     printf("perim=%3.3f\n", perims);
+    return;
+}
+
+void triangle(int xt1, int yt1, int xt2, int yt2, int xt3, int yt3)
+{
+    float p, plosh, perim, str1, str2, str3;
+    ;
+    str1 = sqrt(pow(xt2 - xt1, 2) + pow(yt2 - yt1, 2));
+    str2 = sqrt(pow(xt3 - xt1, 2) + pow(yt3 - yt1, 2));
+    str3 = sqrt(pow(xt3 - xt2, 2) + pow(yt3 - yt2, 2));
+    p = (str1 + str2 + str3) / 2;
+    perim = str1 + str2 + str3;
+    plosh = sqrt(p * (p - str1) * (p - str2) * (p - str3));
+    printf("Plosh=%f\n", plosh);
+    printf("Perim=%f\n", perim);
     return;
 }
 
