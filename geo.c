f5a960d9f9b50c1eff8191981b82defb8306ca2e
dimdimich123 <Frostm333@gmail.com>
2019-03-31 13:47:48 +0700

    Added input figures and output perimeter, square

--- geo.c
+++ geo.c
@@ -147,5 +147,55 @@ int main()
     int x1, u1, x2, u2, x3, u3, p0, r1, r2, r3, f, xt1, xt2, xt3, yt1, yt2, yt3,
             xt4, xt5, xt6, yt4, yt5, yt6, xt7, xt8, xt9, yt7, yt8, yt9;
     int flag, ps2 = 0, ps3 = 0, ps4;
+    bb = 1;
+    while (1) {
+        flag = Str(x1, u1, r1, xt1, yt1, xt2, yt2, xt3, yt3);
+        if (flag)
+            break;
+        if (flag == 3)
+            return 0;
+    }
+    bb = 2;
+    while (1) {
+        flag = Str(x2, u2, r2, xt4, yt4, xt5, yt5, xt6, yt6);
+        if (flag)
+            break;
+        if (flag == 3)
+            return 0;
+    }
+    bb = 3;
+    while (1) {
+        flag = Str(x3, u3, r3, xt7, yt7, xt8, yt8, xt9, yt9);
+        if (flag)
+            break;
+        if (flag == 3)
+            return 0;
+    }
+    puts("\n");
+    if (strncmp(T, FI1, 6) == 0) {
+        puts(FI1);
+        triangle(xt1, yt1, xt2, yt2, xt3, yt3);
+    }
+    if (strncmp(B, FI1, 6) == 0) {
+        puts(FI1);
+        circle(r1);
+    }
+    if (strncmp(T, FI2, 6) == 0) {
+        puts(FI2);
+        triangle(xt4, yt4, xt5, yt5, xt6, yt6);
+    }
+    if (strncmp(B, FI2, 6) == 0) {
+        puts(FI2);
+        circle(r2);
+    }
+    if (strncmp(T, FI3, 6) == 0) {
+        puts(FI3);
+        triangle(xt7, yt7, xt8, yt8, xt9, yt9);
+    }
+    if (strncmp(B, FI3, 6) == 0) {
+        puts(FI3);
+        circle(r3);
+    }
+
     return 0;
 }
