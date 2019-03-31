6cf2bf6dd0cafac371881b29e2f9e30b1af9a0fa
dimdimich123 <Frostm333@gmail.com>
2019-03-31 13:28:01 +0700

    Added coordinates for the vertex of the triangle. Changed global variables to local ones

--- geo.c
+++ geo.c
@@ -2,197 +2,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void Str();
-void circle();
-const char B[] = "circle", E[] = "exit";
-char A[6], C[6], F[6], D[256];
-int x1, u1, x2, u2, x3, u3, p0, r1, r2, r3, f;
-float plosh, perim;
+int Str(int& x1,
+        int& u1,
+        int& r1,
+        int& xt1,
+        int& yt1,
+        int& xt2,
+        int& yt2,
+        int& xt3,
+        int& yt3);
+void circle(int f);
+const char B[] = "circle", E[] = "exit", T[] = "triangle";
+char A[8], C[8], F[8], D[100], FI1[100], FI2[100], FI3[245], H[8], G[8], J[8];
+int bb;
 
 void circle(int f)
 {
-    plosh = 3.14 * f * f;
-    printf("plosh=%3.3f\n", plosh);
-    perim = 2 * 3.14 * f;
-    printf("perim=%3.3f\n", perim);
+    float ploshs, perims;
+    ploshs = 3.14 * f * f;
+    printf("plosh=%3.3f\n", ploshs);
+    perims = 2 * 3.14 * f;
+    printf("perim=%3.3f\n", perims);
     return;
 }
 
-void Str()
+int Str(int& x1,
+        int& u1,
+        int& r1,
+        int& xt1,
+        int& yt1,
+        int& xt2,
+        int& yt2,
+        int& xt3,
+        int& yt3)
 {
-    int i = 0, d = 0, m, k, l, a, n, s, c;
+    int i = 0, d = 0;
     printf("Input String: \n");
     scanf("%s", D);
     if (strncmp(E, D, 4) != 0) {
-        if (strncmp(B, D, 6) != 0) {
-            printf("unknown shape '%s'\n", D);
-            Str();
+        if (strncmp(T, D, 8) != 0) {
+            if (strncmp(B, D, 6) != 0) {
+                printf("unknown shape '%s'\n", D);
+                return 0;
+            } else {
+                if (bb == 1)
+                    strcpy(FI1, D);
+                if (bb == 2)
+                    strcpy(FI2, D);
+                if (bb == 3)
+                    strcpy(FI3, D);
+                for (i = 7; D[i] != ','; i++) {
+                    A[d] = D[i];
+                    if (A[d] == ')') {
+                        printf("invalid input format\n");
+                        return 0;
+                    }
+                    d++;
+                }
+                d = 0;
+                for (i++; D[i] != ','; i++) {
+                    C[d] = D[i];
+                    d++;
+                }
+                d = 0;
+                for (i++; D[i] != ')'; i++) {
+                    F[d] = D[i];
+                    d++;
+                }
+                x1 = atoi(A);
+                u1 = atoi(C);
+                r1 = atoi(F);
+                return 1;
+            }
         } else {
-            for (i = 7; D[i] != ','; i++) {
+            if (bb == 1)
+                strcpy(FI1, D);
+            if (bb == 2)
+                strcpy(FI2, D);
+            if (bb == 3)
+                strcpy(FI3, D);
+            for (i = 9; D[i] != ','; i++) {
                 A[d] = D[i];
                 if (A[d] == ')') {
                     printf("invalid input format\n");
-                    Str();
+                    return 0;
                 }
                 d++;
             }
-            m = d - 1;
             d = 0;
             for (i++; D[i] != ','; i++) {
                 C[d] = D[i];
                 d++;
             }
-            k = d - 1;
             d = 0;
-            for (i++; D[i] != ')'; i++) {
+            for (i++; D[i] != ','; i++) {
                 F[d] = D[i];
                 d++;
             }
-            l = d - 1;
-            for (n = 1, a = 0; m > -1; m--) {
-                if (A[m] == '1')
-                    s = 1;
-                if (A[m] == '2')
-                    s = 2;
-                if (A[m] == '3')
-                    s = 3;
-                if (A[m] == '4')
-                    s = 4;
-                if (A[m] == '5')
-                    s = 5;
-                if (A[m] == '6')
-                    s = 6;
-                if (A[m] == '7')
-                    s = 7;
-                if (A[m] == '8')
-                    s = 8;
-                if (A[m] == '9')
-                    s = 9;
-                if (A[m] == '0')
-                    s = 0;
-                if (A[m] == '-') {
-                    a *= -1;
-                    break;
-                }
-                a += s * n;
-                n *= 10;
+            d = 0;
+            for (i++; D[i] != ','; i++) {
+                H[d] = D[i];
+                d++;
             }
-            for (n = 1, c = 0; k > -1; k--) {
-                if (C[k] == '1')
-                    s = 1;
-                if (C[k] == '2')
-                    s = 2;
-                if (C[k] == '3')
-                    s = 3;
-                if (C[k] == '4')
-                    s = 4;
-                if (C[k] == '5')
-                    s = 5;
-                if (C[k] == '6')
-                    s = 6;
-                if (C[k] == '7')
-                    s = 7;
-                if (C[k] == '8')
-                    s = 8;
-                if (C[k] == '9')
-                    s = 9;
-                if (C[k] == '0')
-                    s = 0;
-                if (C[k] == '-') {
-                    c *= -1;
-                    break;
-                }
-                c += s * n;
-                n *= 10;
+            d = 0;
+            for (i++; D[i] != ','; i++) {
+                G[d] = D[i];
+                d++;
             }
-            for (n = 1, f = 0; l > -1; l--) {
-                if (F[l] == '1')
-                    s = 1;
-                if (F[l] == '2')
-                    s = 2;
-                if (F[l] == '3')
-                    s = 3;
-                if (F[l] == '4')
-                    s = 4;
-                if (F[l] == '5')
-                    s = 5;
-                if (F[l] == '6')
-                    s = 6;
-                if (F[l] == '7')
-                    s = 7;
-                if (F[l] == '8')
-                    s = 8;
-                if (F[l] == '9')
-                    s = 9;
-                if (F[l] == '0')
-                    s = 0;
-                if (F[l] == '-') {
-                    f *= -1;
-                    break;
-                }
-                f += s * n;
-                n *= 10;
+            d = 0;
+            for (i++; D[i] != ')'; i++) {
+                J[d] = D[i];
+                d++;
             }
-            if (p0 == 1) {
-                x1 = a;
-                u1 = c;
-            }
-            if (p0 == 2) {
-                x2 = a;
-                u2 = c;
-            }
-            if (p0 == 3) {
-                x3 = a;
-                u3 = c;
-            }
-            return;
+            xt1 = atoi(A);
+            yt1 = atoi(C);
+            xt2 = atoi(F);
+            yt2 = atoi(H);
+            xt3 = atoi(G);
+            yt3 = atoi(J);
+            return 1;
         }
     } else
-        return;
+        return 3;
 }
 
 int main()
 {
-    float nm;
-    p0 = 1;
-    Str();
-    r1 = f;
-    p0 = 2;
-    Str();
-    r2 = f;
-    p0 = 3;
-    Str();
-    r3 = f;
-
-    printf("\n1.circle(%d,%d,%d)\n", x1, u1, r1);
-    circle(r1);
-    nm = sqrt(pow(x2 - x1, 2) + pow(u2 - u1, 2));
-    printf("intersects:\n");
-    if (nm <= r1 + r2)
-        printf("2.circle(%d,%d,%d)", x2, u2, r2);
-    nm = sqrt(pow(x3 - x1, 2) + pow(u3 - u1, 2));
-    if (nm <= r1 + r3)
-        printf("\n3.circle(%d,%d,%d)\n", x3, u3, r3);
-
-    printf("\n2.circle(%d,%d,%d)\n", x2, u2, r2);
-    circle(r2);
-    nm = sqrt(pow(x1 - x2, 2) + pow(u1 - u2, 2));
-    printf("intersects:\n");
-    if (nm <= r1 + r2)
-        printf("1.circle(%d,%d,%d)", x1, u1, r1);
-    nm = sqrt(pow(x3 - x2, 2) + pow(u3 - u2, 2));
-    if (nm <= r2 + r3)
-        printf("\n3.circle(%d,%d,%d)\n", x3, u3, r3);
-
-    printf("\n3.circle(%d,%d,%d)\n", x3, u3, r3);
-    circle(r3);
-    nm = sqrt(pow(x1 - x3, 2) + pow(u1 - u3, 2));
-    printf("intersects:\n");
-    if (nm <= r1 + r3)
-        printf("1.circle(%d,%d,%d)", x1, u1, r1);
-    nm = sqrt(pow(x3 - x2, 2) + pow(u3 - u2, 2));
-    if (nm <= r2 + r3)
-        printf("\n2.circle(%d,%d,%d)\n", x2, u2, r2);
-
+    int x1, u1, x2, u2, x3, u3, p0, r1, r2, r3, f, xt1, xt2, xt3, yt1, yt2, yt3,
+            xt4, xt5, xt6, yt4, yt5, yt6, xt7, xt8, xt9, yt7, yt8, yt9;
+    int flag, ps2 = 0, ps3 = 0, ps4;
     return 0;
 }
